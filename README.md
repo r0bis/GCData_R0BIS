1b60bc6c098642ef02f70d33c52fecb88531d57a
r0bis <roberts.klotins@gmail.com>
2015-04-26 23:35:50 +0100

    Version 2 of project - with proper summarising

--- README.md
+++ README.md
@@ -1,3 +1,50 @@
-Readme
+Readme GCData_R0BIS
 ======================================================
-This describes what is in this repository.
+This repository contains the coursework for JHU Getting and Cleaning Data course.
+
+The main script which is to be executed from R environment does the following:
+  - it loads the needed libraries - dplyr and sqldf
+  - it looks for data in the subdirectory called 'data'
+  - it produces output in its working directory 'tidydataset.txt'
+
+The dataset you would process is obtained from the following link:
+https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+
+For the run_analysis.R script to work it is necessary that it should contain 
+a subdirectory named dat where you have unzipped the downloaded dataset, preserving 
+the dataset directory structure.
+
+Dataset structure for your reference
+--------------------------------------------------------
+├── activity_labels.txt
+├── features_info.txt
+├── features.txt
+├── README.txt
+├── test
+│   ├── Inertial Signals
+│   │   ├── body_acc_x_test.txt
+│   │   ├── body_acc_y_test.txt
+│   │   ├── body_acc_z_test.txt
+│   │   ├── body_gyro_x_test.txt
+│   │   ├── body_gyro_y_test.txt
+│   │   ├── body_gyro_z_test.txt
+│   │   ├── total_acc_x_test.txt
+│   │   ├── total_acc_y_test.txt
+│   │   └── total_acc_z_test.txt
+│   ├── subject_test.txt
+│   ├── X_test.txt
+│   └── y_test.txt
+└── train
+    ├── Inertial Signals
+    │   ├── body_acc_x_train.txt
+    │   ├── body_acc_y_train.txt
+    │   ├── body_acc_z_train.txt
+    │   ├── body_gyro_x_train.txt
+    │   ├── body_gyro_y_train.txt
+    │   ├── body_gyro_z_train.txt
+    │   ├── total_acc_x_train.txt
+    │   ├── total_acc_y_train.txt
+    │   └── total_acc_z_train.txt
+    ├── subject_train.txt
+    ├── X_train.txt
+    └── y_train.txt
--- run_analysis.R
+++ run_analysis.R
@@ -1,41 +1,45 @@
-# This file should be run from its directory when sub-directory data contains
-# the downloaded raw data set
-# R01 - reading in data and labels:
-# the following libraries are required for data subsetting and SQL selection
+# Version 2.0
+# This file should be run from its directory and the sub-directory 'data'
+# should contain the downloaded raw data set 
+# 2 libraries are required for data subsetting and SQL selection
 library(dplyr)
 library(sqldf)
 
-# read in test data
-Xdt <- read.csv("data/test/X_test.txt",sep="")
-Ylb <- read.csv("data/test/y_test.txt",sep="")
-Sbj <- read.csv("data/test/subject_test.txt",sep="")
 # read in global labels - activity names and feature names
 gALab <- read.csv("data/activity_labels.txt",sep="",header=F)
 colnames(gALab) <- c('ActivityID','ActivityName')
 gFeat <- read.csv("data/features.txt",sep="",header=F)
 colnames(gFeat) <- c('FeatureID','FeatureName')
-# make global feature labels usable for columnnames
-# this is done in sequence to prepare correct names for future column selection
+
+# make global feature labels usable for column names
+# this is done in a particular sequence 
+# column names will be used to select only the needed columns
 gFeat <- as.data.frame(sapply(gFeat,gsub,pattern='-mean\\(\\)-',replacement="Mean"))
 gFeat <- as.data.frame(sapply(gFeat,gsub,pattern='-mean\\(\\)',replacement="Mean"))
 gFeat <- as.data.frame(sapply(gFeat,gsub,pattern='-std\\(\\)-',replacement="Std"))
 gFeat <- as.data.frame(sapply(gFeat,gsub,pattern='-std\\(\\)',replacement="Std"))
+# lastly the unnecessary columns get correct but irrelevant names
 gFeat$FeatureName <- make.names(gFeat$FeatureName, unique=T)
-# insert column names in the Test data set
-colnames(Xdt) <- gFeat$FeatureName
-# now select only columns further needed for work
-XdtTest <- select(Xdt,matches("Mean[XYZ]$|Mean$|Std[XYZ]$|Std$"))
-# now attach subject information to this data set
-XdtTest <- cbind(XdtTest,Sbj[,1])
-colnames(XdtTest)[67] <- c("Subject")
-# now attach activity labels to the data set
-# first prepare descriptive activity names
+
+# read in test data and process this set first
+Xdt <- read.csv("data/test/X_test.txt",sep="")
+Ylb <- read.csv("data/test/y_test.txt",sep="")
+Sbj <- read.csv("data/test/subject_test.txt",sep="")
 colnames(Ylb) <- c('ActivityLabel')
+
+# insert column names in Test data and select only columns further needed
+colnames(Xdt) <- gFeat$FeatureName
+XdtTest <- select(Xdt,matches("Mean[XYZ]$|Mean$|Std[XYZ]$|Std$"))
+# attach subject information to this data set
+XdtTest <- cbind(XdtTest,Sbj[,1])
+colnames(XdtTest)[67] <- c('Subject')
+# attach activity labels to the data set
+# first prepare descriptive activity names
 YlbNames <- sqldf("SELECT gALab.ActivityName
                 FROM Ylb
                 LEFT JOIN gALab
                 ON Ylb.ActivityLabel = gALab.ActivityID")
-# then add the column to the data set
+# then add activity labels to the data set
 XdtTest <- cbind(XdtTest,YlbNames[,1])
 colnames(XdtTest)[68] <- c('Activity')
 
@@ -55,7 +59,13 @@ YlbNames <- sqldf("SELECT gALab.ActivityName
 XdtTrain <- cbind(XdtTrain,YlbNames[,1])
 colnames(XdtTrain)[68] <- c('Activity')
 
-### bind the data frames together
+### bind the processed test and training data frames together
 mrgData <- rbind(XdtTest,XdtTrain)
-# write out the cleran data set
-write.csv(mrgData,file = "./tidyset.csv",row.names=FALSE,col.names=TRUE)
+
+# summarise data - first group, then summarise (mean) on columns grouped by
+groupMrgData <- mrgData %>% group_by(Subject,Activity)
+meanMrgData <- groupMrgData %>% summarise_each(funs(mean))
+
+# write out the clean data set - the requirement is to use write.table()
+# write.csv(mrgData,file = "./tidyset.csv",row.names=FALSE,col.names=TRUE)
+write.table(meanMrgData,file = "./tidydataset.txt",row.names=FALSE,col.names=TRUE,sep=",")
